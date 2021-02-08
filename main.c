06668d711594477837cb1d3a6fae89e7364da42e
Satyendra Yadav <sat.web1989@gmail.com>
2021-02-08 18:14:36 +0530

    by Sanjay

---
+++ main.c
@@ -0,0 +1,16 @@
+#include "helper.h"
+int main(){
+	int i;
+	L:
+	printf("Enter number to get factorial ");
+	scanf("%d", &i);	
+	fflush(stdin);
+	
+	if(i == 0){
+		printf("Please enter valid number %d\n", sizeof i);
+		goto L; 
+	}
+	
+	printf("Factorial of %d is %d", i, factorial(i));
+	printf("Factorial of %d is %d", i, factorial(i));
+}
